5f98c4ff37b30654a162c62095d4aed721e5ff40
amirhossein.shk <amirone13707070>
2019-11-30 23:30:30 +0330

    add some styles

--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -4,7 +4,14 @@ class AddPhoto extends Component {
     render() {
         return (
             <div>
-                <h2>this page must be add photo page.</h2>
+                <h2>Photowall</h2>
+                <div className='form'>
+                    <form>
+                        <input type='text' placeholder='Link'/>
+                        <input type='text' placeholder='Description'/>
+                        <button>POST</button>
+                    </form>
+                </div>
             </div>
         )
     }
